1232e5b62e42937bf1cede3a0c27775f015ffab0
sh4nks <sh4nks7@gmail.com>
2014-05-17 11:45:18 +0200

    Delete hooks.py because we are using the EventManager from Zine now

--- example/hooks.py
+++
@@ -1,9 +0,0 @@
-from flask.ext.plugins import HookManager
-
-# create a hookmanager instance which holds all your hooks
-hooks = HookManager()
-
-# create two new hooks which can than be used in your plugins
-hooks.new("after_navigation")
-hooks.new("tmpl_before_content")  # The "tmpl" prefix marks a template hook.
-hooks.new("tmpl_navigation_last")
