3744b621cfe9a0b7bc290eac3a854244729f62a2
Peter Justin <sh4nks7@gmail.com>
2016-01-26 14:16:04 +0100

    Add missing testcases

--- tests/eventmanager_tests.py
+++ tests/eventmanager_tests.py
@@ -1,10 +1,10 @@
-
 import unittest
 
 from jinja2 import Markup
 from flask import Flask
 
-from flask_plugins import PluginManager, EventManager
+from flask_plugins import PluginManager, EventManager, connect_event, \
+    emit_event, iter_listeners
 
 
 class EventManagerInitTests(unittest.TestCase):
@@ -31,55 +31,77 @@ class EventManagerTests(unittest.TestCase):
         self.plugin_manager = PluginManager(self.app)
         self.event_manager = self.plugin_manager._event_manager
 
-    def callback(self):
-        pass
+    def cb(self):
+        return "Fred"
 
-    def callback_before(self):
+    def cb_before(self):
         pass
 
     def test_event_manager_connect(self):
-        # first connect a callback, than register ah callback on before the
+        # first connect a callback, than register a callback on before the
         # last registered callback
-        self.event_manager.connect('test-event', self.callback)
-        self.event_manager.connect('test-event', self.callback_before, 'before')
+        self.event_manager.connect('test-event', self.cb)                   # 3
+        self.event_manager.connect('test-event', self.cb_before, 'before')  # 1
+        self.event_manager.connect('test-event', self.cb, 'after')          # 4
 
         events = [event for event in self.event_manager.iter('test-event')]
-
-        self.assertEquals(events[0], self.callback_before)
+        self.assertEquals(events[0], self.cb_before)
+        self.assertEquals(events[2], self.cb)
 
     def test_event_manager_iter(self):
         no_events = [event for event in self.event_manager.iter('test-event')]
 
         self.assertEquals(len(no_events), 0)
 
-        self.event_manager.connect('test-event', self.callback)
+        self.event_manager.connect('test-event', self.cb)
 
         events = [event for event in self.event_manager.iter('test-event')]
         self.assertEquals(len(events), 1)
 
     def test_event_manager_remove(self):
-        self.event_manager.connect('test-event', self.callback)
+        self.event_manager.connect('test-event', self.cb)
 
         events = [event for event in self.event_manager.iter('test-event')]
         self.assertEquals(len(events), 1)
 
         # Test if a event is passed which didn't exist
-        self.event_manager.remove('wrong-event', self.callback)
+        self.event_manager.remove('wrong-event', self.cb)
         self.assertEquals(len(events), 1)
 
         # Test if a callback is passed which didn't exist in the event
-        self.event_manager.remove('test-event', self.callback_before)
+        self.event_manager.remove('test-event', self.cb_before)
         self.assertEquals(len(events), 1)
 
         # Test if it actually works
-        self.event_manager.remove('test-event', self.callback)
+        self.event_manager.remove('test-event', self.cb)
 
         events = [event for event in self.event_manager.iter('test-event')]
         self.assertEquals(len(events), 0)
 
     def test_event_manager_template_emit(self):
-        self.event_manager.connect('test-event', self.callback)
+        self.event_manager.connect('test-event', self.cb)
 
         self.assertIsInstance(
             self.event_manager.template_emit('test-event'), Markup
         )
+
+
+class EventManagerHelpersTest(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['TESTING'] = True
+
+        self.plugin_manager = PluginManager(self.app)
+        self.event_manager = self.plugin_manager._event_manager
+
+    def callback(self):
+        return "Fred"
+
+    def test_helper_methods(self):
+        with self.app.test_request_context():
+            connect_event('test-event', self.callback)
+            events = [event for event in iter_listeners('test-event')]
+            emit_result = emit_event('test-event')
+
+        self.assertEquals(len(events), 1)
+        self.assertEquals(emit_result, ["Fred"])
--- tests/pluginmanager_tests.py
+++ tests/pluginmanager_tests.py
@@ -1,9 +1,9 @@
-
+import os
 import unittest
 
 from flask import Flask
 from flask_plugins import PluginManager, PluginError, get_enabled_plugins, \
-    get_plugin
+    get_all_plugins, get_plugin, get_plugin_from_all
 
 
 class InitializationTestCase(unittest.TestCase):
@@ -34,18 +34,22 @@ class PluginManagerTests(unittest.TestCase):
     def test_find_plugins(self):
         found_plugins = self.plugin_manager.find_plugins()
         self.assertEqual(len(found_plugins), 3)
-        expected_plugins = ['TestOnePlugin', 'TestTwoPlugin', 'TestThreePlugin']
+        expected_plugins = ['TestOnePlugin', 'TestTwoPlugin',
+                            'TestThreePlugin']
         self.assertEquals(sorted(found_plugins), sorted(expected_plugins))
 
-    def test_load_plugins(self):
+    def test_plugins(self):
         self.plugin_manager._plugins = None
         self.assertEquals(self.plugin_manager._plugins, None)
 
-        self.plugin_manager.load_plugins()
-
         self.assertEquals(
-            sorted(self.plugin_manager.plugins.keys()), ["test1", "test2"]
+            sorted(self.plugin_manager.plugins.keys()),
+            ["test1", "test2"]
         )
+
+    def test_all_plugins(self):
+        self.plugin_manager._all_plugins = None
+        self.assertEquals(self.plugin_manager._all_plugins, None)
 
         self.assertEquals(
             sorted(self.plugin_manager.all_plugins.keys()),
@@ -61,7 +65,7 @@ class PluginManagerGetPlugins(unittest.TestCase):
         self.plugin_manager = PluginManager()
         self.plugin_manager.init_app(self.app)
 
-    def test_get_plugins_list(self):
+    def test_get_enabled_plugins(self):
         with self.app.test_request_context():
             plugins = get_enabled_plugins()
 
@@ -70,15 +74,26 @@ class PluginManagerGetPlugins(unittest.TestCase):
             set(self.plugin_manager.plugins.values())
         )
 
+    def test_get_all_plugins(self):
+        with self.app.test_request_context():
+            plugins = get_all_plugins()
+
+        self.assertEquals(len(plugins), 3)
+
     def test_get_plugin(self):
         with self.app.test_request_context():
             plugin = get_plugin("test1")
 
         self.assertEquals(plugin, self.plugin_manager.plugins["test1"])
 
+    def test_get_plugin_from_all(self):
+        with self.app.test_request_context():
+            plugin = get_plugin_from_all("test3")
+
+        self.assertFalse(plugin.enabled)
+
 
 class PluginManagerOtherDirectoryTests(unittest.TestCase):
-
     def setUp(self):
         self.app = Flask(__name__)
         self.app.config['TESTING'] = True
@@ -98,7 +113,19 @@ class PluginManagerOnePluginTests(unittest.TestCase):
         self.app.config['TESTING'] = True
         self.plugin_manager = PluginManager()
         self.plugin_manager.init_app(self.app)
-        self.plugin_manager.load_plugins()
+
+    def tearDown(self):
+        # Make sure plugin "test1" is enabled
+        plugin_path = self.plugin_manager.all_plugins["test1"].path
+        if os.path.exists(os.path.join(plugin_path, "DISABLED")):
+            os.remove(os.path.join(plugin_path, "DISABLED"))
+            self.plugin_manager.load_plugins()
+
+    def test_plugin_disable_and_enable(self):
+        plugin = self.plugin_manager.plugins["test1"]
+        self.assertFalse(plugin.disable())
+        plugin = self.plugin_manager.all_plugins["test1"]
+        self.assertTrue(plugin.enable())
 
     def test_plugin_setup(self):
         plugin = self.plugin_manager.plugins["test1"]
@@ -129,23 +156,37 @@ class PluginManagerManagementMethodsTests(unittest.TestCase):
     def test_setup_plugins(self):
         plugin = self.plugin_manager.plugins["test1"]
         self.assertFalse(plugin.setup_called)
-
         self.plugin_manager.setup_plugins()
-
         self.assertTrue(plugin.setup_called)
 
     def test_install_plugins(self):
         plugin = self.plugin_manager.plugins["test1"]
         self.assertFalse(plugin.install_called)
-
         self.plugin_manager.install_plugins()
-
         self.assertTrue(plugin.install_called)
 
     def test_uninstall_plugins(self):
         plugin = self.plugin_manager.plugins["test1"]
         self.assertFalse(plugin.uninstall_called)
-
         self.plugin_manager.uninstall_plugins()
-
         self.assertTrue(plugin.uninstall_called)
+
+
+class PluginManagerEnableDisableTests(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['TESTING'] = True
+        self.plugin_manager = PluginManager()
+        self.plugin_manager.init_app(self.app)
+
+    def test_disable_plugins(self):
+        plugin = self.plugin_manager.plugins["test1"]
+        self.assertTrue(plugin.enabled)
+        self.assertEquals(self.plugin_manager.disable_plugins([plugin]), 1)
+        self.assertTrue(os.path.exists(os.path.join(plugin.path, "DISABLED")))
+
+    def test_enable_plugins(self):
+        plugin = self.plugin_manager.all_plugins["test1"]
+        self.assertFalse(plugin.enabled)
+        self.plugin_manager.enable_plugins([plugin])
+        self.assertTrue(plugin.enabled)
